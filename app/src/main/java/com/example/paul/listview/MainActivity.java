c810bbc94d96c42f42184eba06b30b4a2d9ae19f
Wyns <pauwains@gmail.com>
2015-07-26 09:18:34 +0300

    created listview

--- app/src/main/java/com/example/paul/listview/MainActivity.java
+++ app/src/main/java/com/example/paul/listview/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] Games = {"football", "basketball", "netball", "volleyball"};
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+        Games);
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+                theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String GamesSelected = "you selected" +
+                                String.valueOf(adapterView.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, GamesSelected, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 
     @Override
